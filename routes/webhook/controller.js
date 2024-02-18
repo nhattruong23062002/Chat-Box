5a6bbbc1dc742e156747a065bfeda2a4af01c68c
Nhat <Nhattr2306@gmail.com>
2024-02-18 11:21:24 +0700

    abc

--- routes/webhook/controller.js
+++ routes/webhook/controller.js
@@ -1,41 +1,96 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
   getWebhook: async (req, res, next) => {
-     
-// Parse the query params
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
+    // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
 
-  // Check if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Check the mode and token sent is correct
-    if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
-      // Respond with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      // Respond with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Check the mode and token sent is correct
+      if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+        // Respond with the challenge token from the request
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+      } else {
+        // Respond with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+      }
     }
-  }
   },
 
   createWebhook: async function (req, res, next) {
+    // Parse the request body from the POST
     let body = req.body;
 
-    console.log(`\u{1F7EA} Received webhook:`);
-    console.dir(body, { depth: null });
-
+    // Check the webhook event is from a Page subscription
     if (body.object === "page") {
-        // Returns a '200 OK' response to all requests
-        res.status(200).send("EVENT_RECEIVED");
+      // Iterate over each entry - there may be multiple if batched
+      body.entry.forEach(function (entry) {
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      
+      
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+      
+        // Check if the event is a message or postback and
+        // pass the event to the appropriate handler function
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message);        
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback);
+        }
+      });
 
-        // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
-      } else {
-        // Return a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-      }
+      // Return a '200 OK' response to all events
+      res.status(200).send("EVENT_RECEIVED");
+    } else {
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
   },
 
+  handleMessage: async function (sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {    
+  
+      // Create the payload for a basic text message
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
+    }  
+    
+    // Sends the response message
+    callSendAPI(sender_psid, response); 
+  },
+
+  handlePostback: async function (sender_psid, received_postback) {},
+
+  callSendAPI: async function (sender_psid, response) {
+    let request_body = {
+        "recipient": {
+          "id": sender_psid
+        },
+        "message": response
+      }
+    
+      // Send the HTTP request to the Messenger Platform
+      request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+      }, (err, res, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      }); 
+  },
 };
