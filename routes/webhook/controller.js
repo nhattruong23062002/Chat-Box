d29ed5a39f69cafebffe5000e5e99aa295fcb72c
Nhat <Nhattr2306@gmail.com>
2024-02-18 22:16:38 +0700

    18/2

--- routes/webhook/controller.js
+++ routes/webhook/controller.js
@@ -1,4 +1,48 @@
 require("dotenv").config();
+
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+    };
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
+
+function handlePostback(sender_psid, received_postback) {}
+
+function callSendAPI(sender_psid, response) {
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
+
 module.exports = {
   getWebhook: async (req, res, next) => {
     // Parse the query params
@@ -24,22 +68,20 @@ module.exports = {
     // Parse the request body from the POST
     let body = req.body;
 
-    // Check the webhook event is from a Page subscription
     if (body.object === "page") {
-      // Iterate over each entry - there may be multiple if batched
       body.entry.forEach(function (entry) {
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
-      
-      
+
         // Get the sender PSID
         let sender_psid = webhook_event.sender.id;
-        console.log('Sender PSID: ' + sender_psid);
-      
+        console.log("Sender PSID: " + sender_psid);
+
         // Check if the event is a message or postback and
         // pass the event to the appropriate handler function
         if (webhook_event.message) {
-          handleMessage(sender_psid, webhook_event.message);        
+          console.log("««««« aaaaaaa »»»»»");
+          handleMessage(sender_psid, webhook_event.message);
         } else if (webhook_event.postback) {
           handlePostback(sender_psid, webhook_event.postback);
         }
@@ -51,46 +93,5 @@ module.exports = {
       // Return a '404 Not Found' if event is not from a page subscription
       res.sendStatus(404);
     }
-  },
-
-  handleMessage: async function (sender_psid, received_message) {
-    let response;
-
-    // Check if the message contains text
-    if (received_message.text) {    
-  
-      // Create the payload for a basic text message
-      response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an image!`
-      }
-    }  
-    
-    // Sends the response message
-    callSendAPI(sender_psid, response); 
-  },
-
-  handlePostback: async function (sender_psid, received_postback) {},
-
-  callSendAPI: async function (sender_psid, response) {
-    let request_body = {
-        "recipient": {
-          "id": sender_psid
-        },
-        "message": response
-      }
-    
-      // Send the HTTP request to the Messenger Platform
-      request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": process.env.ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-      }, (err, res, body) => {
-        if (!err) {
-          console.log('message sent!')
-        } else {
-          console.error("Unable to send message:" + err);
-        }
-      }); 
   },
 };
