7bebfbf992c626a5cf4e2ca2ef6898afd02d9783
Nhat <Nhattr2306@gmail.com>
2024-02-18 10:22:28 +0700

    abc

--- routes/webhook/controller.js
+++ routes/webhook/controller.js
@@ -22,7 +22,20 @@ module.exports = {
   },
 
   createWebhook: async function (req, res, next) {
-    
+    let body = req.body;
+
+    console.log(`\u{1F7EA} Received webhook:`);
+    console.dir(body, { depth: null });
+
+    if (body.object === "page") {
+        // Returns a '200 OK' response to all requests
+        res.status(200).send("EVENT_RECEIVED");
+
+        // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+      } else {
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+      }
   },
 
 };
